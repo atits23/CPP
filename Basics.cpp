b65ea23ed8c32b2cf786c6d09493cceac2c8e3ee
atits23 <atitkumar12345@gmail.com>
2024-12-02 00:26:26 +0530

    First time

---
+++ Basics.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include <math.h>
+#include <bits/stdc++.h> //include all libraries
+using namespace std; // help to prevent overrighting of std
+int main(){
+    int x;
+    string s1,s2;
+    cin>>s1>>s2;
+    cin>>x;
+    int len=s1.size();
+
+
+    cout<<"Hey,Atit How aar"<<'\n' ;//use std::endl for same purpose as \n
+    // cout<<x;
+    return 0;
+
+}
